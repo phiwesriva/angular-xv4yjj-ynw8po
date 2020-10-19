883ee0f9a0045dd98028253a971eae57dc624b5b
phiwesriva <phiwes@icloud.com>
2020-10-19 12:01:55 +0200

    4 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+images = ['https://raw.githubusercontent.com/phiwesriva/angular-xv4yjj-cas1wh/master/src/assets/tb1.jpg',
+ 'https://raw.githubusercontent.com/phiwesriva/angular-xv4yjj-cas1wh/master/src/assets/dress.jpg',
+ 'https://raw.githubusercontent.com/phiwesriva/angular-xv4yjj-cas1wh/master/src/assets/tb2.jpg',
+ 'https://raw.githubusercontent.com/phiwesriva/angular-xv4yjj-cas1wh/master/src/assets/tb3.jpg','https://raw.githubusercontent.com/phiwesriva/angular-xv4yjj-cas1wh/master/src/assets/tb4.jpg'];
 
-  constructor() { }
+//.map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 3000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+    config.wrap = true;
+  }
 
   ngOnInit() {
   }
